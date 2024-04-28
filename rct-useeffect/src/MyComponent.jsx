4eacf7a72cf401210f0cca76e8fb6ca7b18fceac
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-28 14:28:32 +0530

    1. useEffect( ) react hook

---
+++ rct-useeffect/src/MyComponent.jsx
@@ -0,0 +1,38 @@
+/* useEffect() = React hook that tells react to DO SOME CODE
+                 when (pick one):
+                 this component re-renders
+                 this component mounts
+                 the state of a value changes */
+/* useEffect(function, [dependencies]) 
+useEffect(() => {})    ===>>>  runs after every re-renders
+useEffect(() => [])    ===>>>  runs only on mounts
+useEffect(() => [value]) ===>>> run on mount + when value changes */ 
+/* USES of useEffect();
+1. Event Listeners
+2. DOM manupulation
+3. subscriptions (real-time updates)
+4. Fetch data from an API
+5. to clean up when the component umounts */
+import React, {useState, useEffect} from 'react';
+
+function MyComponent() {
+    
+    const [count, setCount] = useState(0);
+
+    function addCount() {
+        /* it's always good prcatice to have updater function*/
+        setCount( c => c + 1 );
+    }
+
+
+    return(
+        <>
+            <p> Count: {count} </p>
+            <button onClick={addCount} > add </button>
+        </>
+    );
+}
+export default MyComponent 
+
+
+
